2d4cc79a0e1b2b355f1353b8c1a2a6dbea0e8509
TLIDI Monssef <tlidi.monssef@gmail.com>
2025-10-07 00:09:09 +0100

    Initial commit

---
+++ auto_rebuild.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+PROJECT_DIR="/home/midnom/arithmetic_api"
+IMAGE_NAME="arithmetic-api"
+
+cd "$PROJECT_DIR"
+
+echo "[*] Monitoring for changes in $PROJECT_DIR..."
+while true; do
+  inotifywait -e modify,create,delete -r .
+  echo "[*] Change detected! Rebuilding Docker image..."
+  docker build -t $IMAGE_NAME .
+  docker stop $IMAGE_NAME-container 2>/dev/null
+  docker rm $IMAGE_NAME-container 2>/dev/null
+  docker run -d --name $IMAGE_NAME-container -p 5000:5000 $IMAGE_NAME
+done
